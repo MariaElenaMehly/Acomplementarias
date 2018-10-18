4767f68c92020c0b90094de7e289ef38025a1c3c
MariaElenaMehly <26mariaelena12@gmail.com>
2018-10-17 21:18:30 -0500

    Primer commit

---
+++ grid/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Creditos comlementarios</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<header>
+		<div class="container">
+		<img src="imagenes/logo4.png"  WIDTH="900" HEIGHT="70">	
+		<img src="imagenes/logo3.jpg"  WIDTH="100" HEIGHT="70">
+		</div>	
+	</header>
+<div id="content">
+    <div id="sidebar">
+	<form>
+	<div class="jumbotron">
+
+	</br>
+    <label for="usuario"><b>Usuario:</b></label>
+    <input type="text" class="form-group" placeholder="Nombre de usuario" name="usuario" required>
+
+    <label for="psw"><b>Contraseña:</b></label>
+    <input type="password" class="form-group" placeholder="Contraseña" name="psw" required>
+
+    <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+    
+  </div>
+	</form>
+    </div>
+</div>
+	<div class="container">
+	<section class="main">
+		<article class="col-xs-12 col-md- col-md-9">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="imagenes/ima1.jpg?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imagenes/ima2.jpg?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imagenes/ima3.jpg?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+		</article>
+
+		
+	</section>	
+	</div>
+	
+	
+</body>
+</html>
